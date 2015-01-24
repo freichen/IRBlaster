c4f1210a050ddb608a81039ea20a9b42a2a15b6d
daniel <rubbish@kefa.co.uk>
2015-01-24 14:43:01 +0000

    fix to send play after power event if playing

--- Plugin.pm
+++ Plugin.pm
@@ -1358,8 +1358,8 @@ sub commandCallback {
 
 	my $client = $request->client();
 
-	$log->debug( "*** IR-Blaster: commandCallback() p0: " . $request->{'_request'}[0] . "\n");
-	$log->debug( "*** IR-Blaster: commandCallback() p1: " . $request->{'_request'}[1] . "\n");
+	$log->debug( "Command triggered: " . $request->{'_request'}[0]);
+	$log->debug( "Subcommand triggered: " . $request->{'_request'}[1]);
 	
 	# IRBlaster works with: SB2/3, Transporter and Fab4
 	if( !defined( $client) || !( ( $client->model() eq 'squeezebox2') || ( $client->model() eq 'transporter') || ( $client->model() eq 'fab4'))) {
