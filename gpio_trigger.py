257109359fabe9a3859c30073ff044389d477e94
daniel <rubbish@kefa.co.uk>
2015-01-25 11:59:32 +0000

    enable gpio trigger script to turn on and off

--- gpio_trigger.py
+++ gpio_trigger.py
@@ -7,6 +7,8 @@ import sys
 import time
 import subprocess
 
+print "Running ", sysargv[0], " script."
+
 # slightly inelegant way of setting variables if they exist
 if sys.argv[1]:
 	client_name = sys.argv[1]
